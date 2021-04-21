f999c792178ec4e16bf792241745b3dc62b1bf6c
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-04-20 22:22:31 -0500

    comienzo del movil meny y formulario de busqueda

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 /*Menu hamburguesa animado*/
 
-const hamburguer_icon = document.querySelector('.hamburger-icon');
-
-hamburguer_icon.addEventListener('click', animatedHamburguerIcon);
-
-function animatedHamburguerIcon(e)
+if(document.querySelector('.hamburger-icon__container'))
 {
-    this.classList.toggle('actived__hamburguer');
+    const hamburguer_icon = document.querySelector('.hamburger-icon__container');
+    hamburguer_icon.addEventListener('click', animatedHamburguerIcon);
+
+    function animatedHamburguerIcon()
+    {
+        this.querySelector('.hamburger-icon').classList.toggle('actived__hamburguer');
+    }
+}
+
+/*Formulario de búsqueda*/
+
+if(document.querySelector('#category__search__menu'))
+{
+    const category_container = document.querySelector('#category__search__menu')
+    category_container.addEventListener('click', displayCategories);
+
+    function displayCategories()
+    {
+        this.classList.toggle('actived');
+    }
 }
