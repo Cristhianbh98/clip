8da3a7e2a42bdc28e678ecbee62c20c2fc14fe07
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-04-23 15:23:23 -0500

    buscador actualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,45 @@ if(document.querySelector('.hamburger-icon__container'))
 
 if(document.querySelector('#category__search__menu'))
 {
-    const category_container = document.querySelector('#category__search__menu')
-    category_container.addEventListener('click', displayCategories);
+    const category_container = document.querySelector('#category__search__menu');
+    const span_button_toggle = category_container.querySelector('span.category');
+    const category_pick_container = category_container.querySelector('div.category-pick');
+    
+    /*Events*/
+    span_button_toggle.addEventListener('click', displayCategories);
+    category_pick_container.addEventListener('click', (e) => { checkedLabel(e); });
 
+    /*Funciones*/
     function displayCategories()
     {
-        this.classList.toggle('actived');
+        category_container.classList.toggle('actived');
+    }
+
+    function checkedLabel(e)
+    {
+        if(e.target.classList.contains('radio__indicator') || e.target.classList.contains('radio__link') || e.target.classList.contains('radio-container'))
+        {
+            //Eliminar la clase checked del elemento seleccionado
+            this.document.querySelector('label.checked').classList.toggle('checked');
+
+            //Actualizar el elemento seleccionado
+            const label = findNearestParentLabel(e.target);
+            label.classList.toggle('checked');
+
+            //Actualizar el texto del elemento span.category
+            span_button_toggle.textContent = label.querySelector('.radio__link').textContent;
+            console.log(label.querySelector('.radio__link').textContent);
+        }
+    }
+
+    //Buscar el label
+    function findNearestParentLabel(el)
+    {
+        let label = el;
+        while(label.tagName != 'LABEL')
+        {
+            label = label.parentElement
+        }
+        return label;
     }
 }
