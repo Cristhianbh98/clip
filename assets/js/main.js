894dbbcb65cb19edc09ff1a6cf73ad4485d14aa4
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-04-25 13:05:00 -0500

    inicio de producto

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,34 @@ if(document.querySelector('#category__search__menu'))
     }
 }
 
+/*--------------------------------------------*/
+//Lista de categorias de los productos
+/*--------------------------------------------*/
+
+if(document.getElementById('category_select'))
+{
+    const category_container = document.getElementById('category_select');
+
+    /*Events*/
+
+    category_container.addEventListener('click', (e) => selectCategory(e));
+
+    /*Functions*/
+
+    function selectCategory(e)
+    {
+        if(e.target.classList.contains('btn__category'))
+        {
+            const button_category = e.target;
+            if(!button_category.classList.contains('checked'))
+            {
+                category_container.querySelector('.checked').classList.toggle('checked');
+                document.querySelector('span.category__selected').textContent = button_category.textContent;
+                button_category.classList.toggle('checked');
+            }
+        }
+    }
+}
 
 /*--------------------------------------------*/
 //Lista de categorias de los productos - celular
@@ -83,17 +111,37 @@ if(document.getElementById('category_select__mobile'))
 {
     const category_mobile_container = document.getElementById('category_select__mobile')
     const category_selected = category_mobile_container.querySelector('span.category__selected');
+    const all_categories_container = category_mobile_container.querySelector('.all__btn__categories');
 
     /*Events*/
 
     category_selected.addEventListener('click', displayAllCategoriesMobile);
+    all_categories_container.addEventListener('click', (e)=> selectCategoryMobile(e));
 
     /*Functions*/
 
+    function selectCategoryMobile(e)
+    {
+        if(e.target.classList.contains('btn__category'))
+        {
+            const button_category = e.target;
+
+            category_selected.textContent = button_category.textContent;
+            all_categories_container.style.maxHeight = null;
+
+            const container_categories = document.getElementById('category_select');
+            const all_buttons_category = container_categories.querySelectorAll('button.btn.btn__category');
+
+            container_categories.querySelector('.checked').classList.toggle('checked');
+
+            all_buttons_category.forEach((item) => {
+                if(item.textContent == category_selected.textContent) item.classList.toggle('checked');
+            });
+        }
+    }
+
     function displayAllCategoriesMobile()
     {
-        const all_categories_container = category_mobile_container.querySelector('.all__btn__categories');
-
         if(all_categories_container.style.maxHeight)
         {
             all_categories_container.style.maxHeight = null;
